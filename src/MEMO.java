9767423faf6cc6a2f20d8c967523d751d664d3a3
YamaRascal <s0304yamatatsu@gmail.com>
2014-07-06 11:44:31 +0900

    third commit

--- src/MEMO.java
+++ src/MEMO.java
@@ -1,12 +1,12 @@
 
 public class MEMO {
-	/*
+	/*仕様１
+	 * String titleをrecipi_titleに変更
+	 * こっちの方が伝わる？
 	 * 
 	 * 
-	 * 
-	 * 
-	 * 
-	 * 
+	 * 仕様３
+	 * メソッドを分けて見やすくしたつもり
 	 * 
 	 * 
 	 * 
--- src/main.java
+++ src/main.java
@@ -1,15 +1,44 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 public class main {
 
-	public static void main(String[] args) {
+	String recipi_title ;
+	String file_name;
+
+	public static void main(String[] args) throws IOException {
 		// TODO 自動生成されたメソッド・スタブ
+		main m = new main();
 		Scanner scan = new Scanner(System.in);
+
+		m.newFile(scan);
+		m.importRecipi(scan);
+		m.FileImportRecipi();
 		
-		String title ;
-		
-		System.out.print("レシピ名＞");
-		title = scan.next();
-		System.out.println(title);
+		/*
+
+
+		 */
+	}
+	void newFile(Scanner scan) throws IOException{
+		System.out.print("新しくレシピ名を入力するファイルを作成します\nファイル名を入力してください >");
+		file_name = scan.next();
+
+		File newfile = new File(file_name+".txt");
+		newfile.createNewFile();
+	}
+
+	void importRecipi(Scanner scan){
+		System.out.print("レシピ名を入力してください >");
+		recipi_title = scan.next();
+		System.out.println(recipi_title);
+	}
+	void FileImportRecipi() throws IOException{
+		FileWriter filewriter = new FileWriter(file_name+".txt");
+
+		filewriter.write(recipi_title);
+		filewriter.close();
 	}
 
 }
