f815b7a3278641b732ffcda3a40a273cf31ce4ab
YamaRascal <s0304yamatatsu@gmail.com>
2014-07-06 11:01:42 +0900

    second commit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+		
+		String title ;
+		
+		System.out.print("レシピ名＞");
+		title = scan.next();
+		System.out.println(title);
+	}
+
+}
